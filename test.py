26be37450c5e520bef7f81959e6bba416cbe5c0c
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-05-28 00:38:11 -0700

    found best script so far to use for live face recognition

--- test.py
+++ test.py
@@ -1,35 +1,192 @@
-from deepface import DeepFace
+import face_recognition
+import cv2
+import numpy as np
 
-models = [
-  "VGG-Face", 
-  "Facenet", 
-  "Facenet512", 
-  "OpenFace", 
-  "DeepFace", 
-  "DeepID", 
-  "ArcFace", 
-  "Dlib", 
-  "SFace",
-  "GhostFaceNet",
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("template.jpeg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+]
+known_face_names = [
+    "Barack Obama",
 ]
 
-# face verification
-try:
-    verification = DeepFace.verify(img1_path = "template.jpeg", img2_path = "tocheck.jpeg")
-    print("\nFace Verified: " + (str)(verification["verified"]) + "\n")
-except ValueError:
-    print("\nNo face detected in the image\n")
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
 
-# analyze image
-try:
-    analysis = DeepFace.analyze(img_path = "template.jpeg", actions = ["age", "gender", "emotion", "race"])
-    print("\nAge:     " + (str)(analysis[0]["age"]))
-    gender = analysis[0]["gender"]
-    print("Gender:  " + (str)(max(gender, key=gender.get)))
-    print("Emotion: " + (str)(analysis[0]["dominant_emotion"]))
-    print("Race:    " + (str)(analysis[0]["dominant_race"]))
-except ValueError:
-    print("\nNo face detected in the image")
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
 
-# realtime face recognition
-DeepFace.stream(db_path = "me", model_name = models[2])
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+        
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+
+# import face_recognition
+# import matplotlib.pyplot as plt
+
+# image = face_recognition.load_image_file("tocheck.jpeg")
+# face_locations = face_recognition.face_locations(image)
+
+# for face_location in face_locations:
+#     top, right, bottom, left = face_location
+
+#     plt.imshow(image[top:bottom, left:right])
+#     plt.show()
+    
+
+
+# import cv2
+# import time
+# from deepface import DeepFace
+
+# models = [
+#   "VGG-Face", 
+#   "Facenet", 
+#   "Facenet512", 
+#   "OpenFace", 
+#   "DeepFace", 
+#   "DeepID", 
+#   "ArcFace", 
+#   "Dlib", 
+#   "SFace",
+#   "GhostFaceNet",
+# ]
+
+# face_classifier = cv2.CascadeClassifier(
+#     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+# )
+
+# video_capture = cv2.VideoCapture(0)
+
+# def detect_bounding_box(vid):
+#     gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+#     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
+#     for (x, y, w, h) in faces:
+#         cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+#     return faces
+
+# while True:
+#     result, video_frame = video_capture.read()  # read frames from the video
+#     if result is False:
+#         break  # terminate the loop if the frame is not read successfully
+
+#     faces = detect_bounding_box(video_frame)  # apply the function we created to the video frame
+#     print(faces)
+
+#     # face verification
+#     try:
+#         verification = DeepFace.verify(
+#             img1_path = "people/joey/joey.jpeg",
+#             img2_path = "tocheck.jpeg",
+#             model_name = models[2])
+#         print("\nFace Verified: " + (str)(verification["verified"]) + "\n")
+#     except ValueError:
+#         print("\nNo face detected in the image\n")
+
+#     cv2.imshow(
+#         "My Face Detection Project", video_frame
+#     )  # display the processed frame in a window named "My Face Detection Project"
+
+#     if cv2.waitKey(1) & 0xFF == ord("q"):
+#         break
+
+# video_capture.release()
+# cv2.destroyAllWindows()
+
+# # # face verification
+# # try:
+# #     verification = DeepFace.verify(img1_path = "template.jpeg", img2_path = "tocheck.jpeg")
+# #     print("\nFace Verified: " + (str)(verification["verified"]) + "\n")
+# # except ValueError:
+# #     print("\nNo face detected in the image\n")
+
+# # # analyze image
+# # try:
+# #     analysis = DeepFace.analyze(img_path = "template.jpeg", actions = ["age", "gender", "emotion", "race"])
+# #     print("\nAge:     " + (str)(analysis[0]["age"]))
+# #     gender = analysis[0]["gender"]
+# #     print("Gender:  " + (str)(max(gender, key=gender.get)))
+# #     print("Emotion: " + (str)(analysis[0]["dominant_emotion"]))
+# #     print("Race:    " + (str)(analysis[0]["dominant_race"]))
+# # except ValueError:
+# #     print("\nNo face detected in the image")
+
+# # # realtime face recognition
+# # DeepFace.stream(db_path = "people", model_name = models[2])
